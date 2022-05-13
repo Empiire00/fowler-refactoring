608cd87d25725cd2a9f0b7613308800f275a9d3f
Tobias Kreuss <tobiaskreuss@hotmail.de>
2022-05-13 11:59:33 +0200

    refactor: extracted methods in Customer

--- src/classes/Customer.java
+++ src/classes/Customer.java
@@ -18,12 +18,16 @@ public class Customer {
     };
 
     public String statement() {
+        String statement = getRentalRecordHeader();
+        statement += getRentalRecord();
+        return statement;
+    }
+
+    private String getRentalRecord() {
+        String rentalRecord = "";
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
-        String result = "Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
-
+        Enumeration enum_rentals = rentals.elements();
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
@@ -31,16 +35,22 @@ public class Customer {
             thisAmount = amountFor(each);
             frequentRenterPoints = addFrequentRenterPoints(frequentRenterPoints);
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints = addFrequentRenterPoints(frequentRenterPoints);
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            rentalRecord = "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
+        rentalRecord += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        rentalRecord += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return rentalRecord;
+    }
+
+    private String getRentalRecordHeader() {
+        String header = "Rental Record for " + this.getName() + "\n";
+        header += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        return header;
     }
 
     private static int addFrequentRenterPoints(int frequentRenterPoints) {
