83444b26d0765324876bc320bf58ff34890fec46
Tobias Kreuss <tobiaskreuss@hotmail.de>
2022-05-13 13:58:27 +0200

    refactor: added MoviePriceCategory

--- src/classes/Movie.java
+++ src/classes/Movie.java
@@ -1,23 +1,26 @@
 package classes;
 
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
+    private MoviePriceCategory priceCategory;
 
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         priceCode = newpriceCode;
+        priceCategory = MoviePriceCategory.values()[newpriceCode];
     }
 
     public int getPriceCode() {
         return priceCode;
     }
+    public MoviePriceCategory getPriceCategory(){
+        return priceCategory;
+    }
 
     public void setPriceCode(int arg) {
         priceCode = arg;
+        priceCategory = MoviePriceCategory.values()[priceCode];
     }
 
     public String getTitle (){
--- src/classes/Rental.java
+++ src/classes/Rental.java
@@ -19,16 +19,16 @@ public class Rental {
 
     public double getAmountFor() {
         double thisAmount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+        switch (getMovie().getPriceCategory()) {
+            case REGULAR:
                 thisAmount += 2;
                 if (getDaysRented() > 2)
                     thisAmount += (getDaysRented() - 2) * 1.5;
                 break;
-            case Movie.NEW_RELEASE:
+            case NEW_RELEASE:
                 thisAmount += getDaysRented() * 3;
                 break;
-            case Movie.CHILDRENS:
+            case CHILDRENS:
                 thisAmount += 1.5;
                 if (getDaysRented() > 3)
                     thisAmount += (getDaysRented() - 3) * 1.5;
